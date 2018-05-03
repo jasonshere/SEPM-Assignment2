663f271534fd47d2b16d283cd26fe9d09f675dfe
JasonLee <s3620273@student.rmit.edu.au>
2018-05-03 15:58:36 +1000

    modify the format of the data file

--- sprint1/controllers/SiteController.php
+++ sprint1/controllers/SiteController.php
@@ -35,42 +35,6 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-
-
-        $movies = [
-            [
-                'id' => 1,
-                'theater' => 'Hall#1',
-                'title' => '3D Avengers',
-                'subtitle' => '3D Avengers',
-                'post' => 'holder.js/400x300',
-                'price' => '$22.99',
-                'describtion' => '',
-                'playing_date' => [
-                    'Monday',
-                    'Tuesday',
-                    'Wednesday'
-                ],
-                'playing_time' => [
-                    '11:00AM - 02:00PM',
-                    '02:00PM - 05:00PM',
-                    '05:00PM - 08:00PM',
-                    '08:00PM - 11:00PM',
-                ],
-                'seats' => [
-                    [
-                        'Monday 11:00AM - 02:00PM' => [
-                            '05', '08', '10'
-                        ],
-                    ],
-                ],
-            ]
-        ];
-
-        $str = json_encode($movies);
-        
-        file_put_contents("/home/wwwroot/sepm/sprint1/data/sepm.dat", $str);
-
         return $this->render('index');
     }
 
