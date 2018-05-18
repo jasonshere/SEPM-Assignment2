fd016fa031582e0872db84fa123739d88cbd3504
JasonLee <s3620273@student.rmit.edu.au>
2018-05-18 17:01:22 +1000

    modify the email template

--- sprint3/mail/pay/email.php
+++ sprint3/mail/pay/email.php
@@ -2,5 +2,5 @@
     <p>Dear, <?php echo $name; ?>:</p>
     <p>Thank you for your booking on SEPM Movie Website!</p>
     <p>This is your Ticket Code: <span style="color:red;background-color:yellow"><?php echo $ticket_code; ?></span></p>
-    <p>Please show it to our staff when you arrived at our Cinema!</p>
+    <p>Please show it to our staff when you arrived at our Aurora Cinemas!</p>
 </div>
